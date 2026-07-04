1eca8e9caf8be06cdedaf3f7f961c6c4bf705282
Syed <syed786000000@gmail.com>
2026-07-05 01:17:51 +0530

    Complete expense tracker version 1

--- main.py
+++ main.py
@@ -1,13 +1,20 @@
 import os
 filename="expenses.txt"
+
+def clear_screen():
+    if os.name == "nt":
+        os.system("cls")
+    else:
+        os.system("clear")
+
 def add_expense():
     category=input("Category: ")
-    amount=input("Amount: ")
-    with open("expenses.txt","a") as file:
+    amount=float(input("Amount: "))
+    with open("filename","a") as file:
         file.write(f"{category},{amount}\n")
 
 def view_expenses():
-    with open("expenses.txt","r") as file:
+    with open("filename","r") as file:
         for line in file:
             line=line.strip()
             parts=line.split(",")
@@ -15,10 +22,9 @@ def view_expenses():
 
 def show_total():
     total=0
-    with open("expenses.txt","r") as file:
+    with open("filename","r") as file:
         for line in file:
-            line=line.strip()
-            parts=line.split(",")
+            parts=line.strip().split(",")
             price=float(parts[1])
             total+=price
     print(f"Total expenses: {total}")
@@ -26,28 +32,81 @@ def show_total():
 def search_category():
     search=input("Enter the category to search for: ")
     found=False
-    with open("expenses.txt","r") as file:
+    with open("filename","r") as file:
         for line in file:
-            line=line.strip()
-            parts=line.split(",")
+            parts=line.strip().split(",")
             if search.lower() == parts[0].lower():
                 print(f"{parts[0]} - {parts[1]}")
                 found=True
         if not found:
             print("No expenses found.")
 
+def delete_category():
+    delete=input("Enter category: ")
+    found=[]
+    with open("filename","r") as file:
+        lines = file.readlines()
 
+        for index,line in enumerate(lines):
+            parts=line.strip().split(",")
+            if delete.lower() == parts[0].lower():
+                found.append((index, parts))
+
+        if not found:
+            print("No such expenses found.")
+            return
+        
+        print("Which expence to delete? ")
+        for i,(original_index, parts) in enumerate(found, start=1):
+            print(f"{i}. {parts[0]} - {parts[1]}")
+        
+        choice=int(input("Enter number: "))
+        if choice < 1 or choice > len(found):
+            print("Invalid choice!!")
+            return
+        line_to_delete = found[choice-1][0]
+
+        lines.pop(line_to_delete)
+
+        with open("filename", "w") as file:
+                for line in lines:
+                    file.write(line)
+                print("Expense deleted successfully.")
+
+
+    
 
 def main():
-    os.system('clear' if os.name != 'nt' else 'cls')
+    while True:
+        clear_screen()
 
-    print("""1. Add Expense
+        print("""===== Expense Tracker =====
+
+1. Add Expense
 2. View Expenses
 3. Show Total
 4. Search by Category
-5. Exit""")
+5. Delete expense by Category
+6. Exit
+""")
 
-    choise=input("Enter choise: ")
+        choice = input("Enter choice: ")
 
+        if choice == "1":
+            add_expense()
+        elif choice == "2":
+            view_expenses()
+        elif choice == "3":
+            show_total()
+        elif choice == "4":
+            search_category()
+        elif choice == "5":
+            delete_category()
+        elif choice == "6":
+            print("Goodbye!")
+            break
+        else:
+            print("Invalid choice!")
 
+        input("\nPress Enter to continue...")
 main()
